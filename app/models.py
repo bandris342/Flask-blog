1229da3b1ec2ab411287bf268eddeb3e8170f43f
bandris342 <andras.bartok@gmail.com>
2018-03-18 22:04:31 +0100

    added User model class

--- app/models.py
+++ app/models.py
@@ -4,8 +4,19 @@ db = SQLAlchemy()
 
 
 class Articles(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.String(300))
-    description = db.Column(db.String(1000))
-    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    __tablename__ = "Articles"
+    id = db.Column('article_id', db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column('title', db.String(300))
+    description = db.Column('description', db.String(1000))
+    created_at = db.Column('created_at', db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column('updated_at', db.DateTime)
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column('user_id', db.Integer, primary_key=True)
+    username = db.Column('username', db.String(20), unique=True)
+    password = db.Column('password', db.String(10))
+    registered_on = db.Column('registered_on', db.DateTime)
+
 
